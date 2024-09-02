81441be55158a6ccb2a43606e96c9710f2b2a023
huyycp <baohuyvongoc1@gmail.com>
2024-09-02 15:30:13 +0700

    feat: [MB][UI] set up theme

---
+++ mobile/lib/theme/color_scheme.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const PRIMARY_COLOR = Color(0xFF1ED760);
+const PRIMARY_BACKGROUND = Color(0xFF121212);
+const BODY_TEXT = Color(0xFFB8B8B8);
+const LABEL_COLOR = Color(0xFFB3B3B3);
+const GRAY_BUTTON = Color(0xFF2A2A2A);
+const GREEN_BUTTON = Color(0xFF14833B);
+const GRAY_BCK_1 = Color(0xFF282828);
+const GRAY_BCK_2 = Color(0xFF444444);
+const DIVIDER = Color(0xFF1B1B1B);
+const TRACK_LINE = Color(0xFF2A2A2A);
+const BUTTON_STROKE = Color(0xFF7F7F7F);
