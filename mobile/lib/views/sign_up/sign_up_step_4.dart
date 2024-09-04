b59ee3ab0d9bc38aecbdc08ca9e95ebb40093070
huyycp <baohuyvongoc1@gmail.com>
2024-09-05 00:06:34 +0700

    feat: [MB][UI] add role selector and update password rule

--- mobile/lib/views/sign_up/sign_up_step_4.dart
+++ mobile/lib/views/sign_up/sign_up_step_4.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:mobile/theme/color_scheme.dart';
 import 'package:mobile/utils/validators.dart';
 import 'package:mobile/views/sign_up/sign_up_view_model.dart';
+import 'package:mobile/views/sign_up/widgets/forward_button.dart';
 
 class SignUpStep4 extends ConsumerStatefulWidget {
   const SignUpStep4({super.key});
@@ -40,19 +41,21 @@ class _SignUpStep4State extends ConsumerState<SignUpStep4> {
       padding: const EdgeInsets.all(30),
       child: Column(
         children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('What\'s your name?', style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
-                _nameInput(),
-                const SizedBox(height: 8),
-                const Text('This appears on your profile ', style: TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.w400),),
-                const SizedBox(height: 20),
-              ],        
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('What\'s your name?', style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+              _nameInput(),
+              const SizedBox(height: 8),
+              const Text('This appears on your profile ', style: TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.w400),),
+            ],        
           ),
-          _forwardBtn(),
+          const SizedBox(height: 20),
+          ForwardButton(
+            destination: '/sign-up/step-5',
+            currentFormKey: ref.read(signUpViewModel).nameFormKey,
+          ),
         ],
       )
     );
@@ -74,27 +77,6 @@ class _SignUpStep4State extends ConsumerState<SignUpStep4> {
           filled: true,
         ),
       ),
-    );
-  }
-
-  Widget _forwardBtn() {
-    return  ElevatedButton(
-      onPressed: () {
-        if (ref.read(signUpViewModel).nameFormKey.currentState!.validate()) {
-          context.go('/home');
-          print(ref.read(signUpViewModel).emailController.text);
-          print(ref.read(signUpViewModel).passwordController.text);
-          print(ref.read(signUpViewModel).genderController.text);
-          print(ref.read(signUpViewModel).nameController.text);
-        }
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black,
-        disabledForegroundColor: Colors.black,
-        backgroundColor: Color(0xFFFFFFFF),
-        disabledBackgroundColor: Color(0xFF535353),
-      ),
-      child: const Text('Create account'),
     );
   }
 }
