2afc021c85a8d68973e516da232bafd4c18a0bd0
huyycp <baohuyvongoc1@gmail.com>
2024-09-06 19:37:25 +0700

    fix: [MB][API] encrypt token

--- mobile/lib/data/data_sources/magic_music_api.dart
+++ mobile/lib/data/data_sources/magic_music_api.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile/data/data_sources/base_api.dart';
@@ -10,4 +11,35 @@ class MagicMusicApi extends BaseApi {
   MagicMusicApi(String baseUrl) {
     dio.options.baseUrl = baseUrl;
   }
+
+  /// Handle requests
+  Future<Response> request(
+    String path, {
+    required HttpMethod method,
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+  }) async {
+    try {
+      final response = await dio.request(
+        path, 
+        data: data,
+        queryParameters: queryParameters,
+        options: (options?..method=method.name) ?? Options(method: method.name),
+        cancelToken: cancelToken,
+      );
+      return response;
+    } catch (e) {
+      rethrow; 
+    }
+  }
+}
+
+enum HttpMethod {
+  GET,
+  POST,
+  PUT,
+  PATCH,
+  DELETE
 }
