5858c8377b8731d6e19319779dd49ae1faf33c55
huyycp <baohuyvongoc1@gmail.com>
2024-09-06 19:57:11 +0700

    feat: [MB][RG] register api

---
+++ mobile/lib/data/data_sources/user_remote_data_source.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile/data/data_sources/magic_music_api.dart';
+import 'package:mobile/data/dto/register_dto.dart';
+
+final userRemoteProvider = Provider<UserRemoteDataSource>(
+  (ref) => UserRemoteDataSource(ref.read(magicMusicApiProvider))
+);
+
+class UserRemoteDataSource {
+  UserRemoteDataSource(MagicMusicApi magicMusicApi) {
+    _magicMusicApi = magicMusicApi;
+  }
+
+  late MagicMusicApi _magicMusicApi;
+  final String _registerPath = '/';
+
+  Future<bool> registerWithEmail(RegisterDto dto) async {
+    final response = await _magicMusicApi.request(
+      _registerPath, 
+      method: HttpMethod.POST,
+      data: dto.toJson()
+    );
+    
+    if (response.statusCode == HttpStatus.created) {
+      final data = response.data;
+      final accessToken = data['access_token'] ?? '';
+      _magicMusicApi.setAccessToken(accessToken);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ mobile/lib/data/dto/register_dto.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class RegisterDto {
+  RegisterDto({
+    required this.email,
+    required this.password,
+    required this.gender,
+    required this.name,
+    required this.role
+  });
+  
+  String email;
+  String password;
+  String gender;
+  String name;
+  UserRole role;
+
+  Map<String, dynamic> toJson() => {
+    'email': email,
+    'password': password,
+    'gender': gender,
+    'name': name,
+    'role': role.name
+  };
+}
+
+enum UserRole {
+  listener,
+  artist
+}
