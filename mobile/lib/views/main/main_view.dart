692a5740c867bd3c34f488cf43d1411f84f117a6
huyycp <baohuyvongoc1@gmail.com>
2024-09-09 22:37:03 +0700

    feat: [MB][UI] Navbar

---
+++ mobile/lib/views/main/main_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile/views/main/main_view_model.dart';
+import 'package:mobile/widgets/dynamic_image.dart';
+
+class MainView extends ConsumerStatefulWidget {
+  const MainView({super.key});
+
+  @override
+  ConsumerState<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends ConsumerState<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+      bottomNavigationBar: _bottomNavBar(),
+    );
+  }
+
+  Widget _body() {
+    return ref.watch(mainViewModel.select(
+      (value) => value.views[value.currentPage]
+    ));
+  }
+
+  Widget _bottomNavBar() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          colors: [Colors.black, Colors.black.withOpacity(0.75)]
+        ),
+      ),
+      child: BottomNavigationBar(
+        currentIndex: ref.watch(mainViewModel).currentPage,
+        unselectedLabelStyle: TextStyle(
+          color: Colors.white
+        ),
+        selectedLabelStyle: TextStyle(
+          color: Colors.white
+        ),  
+        useLegacyColorScheme: false,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        backgroundColor: Colors.transparent,
+        onTap: ref.read(mainViewModel).changeView,
+        items: ref.read(mainViewModel).viewInfos.map(
+          (view) => BottomNavigationBarItem(
+            label: view.title,
+            backgroundColor: Colors.transparent,
+            icon: DynamicImage(view.iconData, width: 24, height: 24),
+            activeIcon: DynamicImage(view.selectedIconData, width: 24, height: 24),
+        )).toList()
+      )
+    );
+  }
+}
