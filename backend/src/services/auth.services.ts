3ea58e096c37f7a74c3cda3df82c4712d342156f
Dong Huynh <125482430+winterrr03@users.noreply.github.com>
2024-09-10 20:16:08 +0700

    feat: [BE][AUTH] Auth API (#16)
    * feat: [BE][SU] connect database
    
    * feat: [BE][RG] Register API
    
    * feat: [BE][LI] login API
    
    * feat: [BE][LO] logout API
    
    * feat: [BE][VE] send verify email
    
    * feat: [BE][VE] resend verify email
    
    * feat: [BE][FP] Send mail forgot password
    
    * feat: [BE][FP] verify forgot password token
    
    * feat: [BE][RP] reset password
    
    * feat: [BE][RT] refresh token
    
    * feat: [BE][RG] update register API

---
+++ backend/src/services/auth.services.ts
@@ -0,0 +1,275 @@
+import { ObjectId } from 'mongodb'
+import { envConfig } from '~/constants/config'
+import { TokenType, UserVerifyStatus } from '~/constants/enums'
+import { RegisterReqBody } from '~/models/requests/Auth.requests'
+import { signToken, verifyToken } from '~/utils/jwt'
+import databaseService from './database.services'
+import User from '~/models/schemas/User.schema'
+import { hashPassword } from '~/utils/crypto'
+import RefreshToken from '~/models/schemas/RefreshToken.schema'
+import { AUTH_MESSAGES } from '~/constants/messages'
+import { generateRandomNumber } from '~/utils/generate'
+import { sendEmail } from '~/utils/email'
+
+class AuthService {
+  private signAccessToken({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.AccessToken,
+        verify
+      },
+      privateKey: envConfig.jwtSecretAccessToken,
+      options: {
+        expiresIn: envConfig.accessTokenExpiresIn
+      }
+    })
+  }
+
+  private signRefreshToken({ user_id, verify, exp }: { user_id: string; verify: UserVerifyStatus; exp?: number }) {
+    if (exp) {
+      return signToken({
+        payload: {
+          user_id,
+          token_type: TokenType.RefreshToken,
+          verify,
+          exp
+        },
+        privateKey: envConfig.jwtSecretRefreshToken
+      })
+    }
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.RefreshToken,
+        verify
+      },
+      privateKey: envConfig.jwtSecretRefreshToken,
+      options: {
+        expiresIn: envConfig.refreshTokenExpiresIn
+      }
+    })
+  }
+
+  private signEmailVerifyToken({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.EmailVerifyToken,
+        verify
+      },
+      privateKey: envConfig.jwtSecretEmailVerifyToken,
+      options: {
+        expiresIn: envConfig.emailVerifyTokenExpiresIn
+      }
+    })
+  }
+
+  private signForgotPasswordToken({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.ForgotPasswordToken,
+        verify
+      },
+      privateKey: envConfig.jwtSecretForgotPasswordToken,
+      options: {
+        expiresIn: envConfig.forgotPasswordTokenExpiresIn
+      }
+    })
+  }
+
+  private signAccessAndRefreshToken({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
+    return Promise.all([this.signAccessToken({ user_id, verify }), this.signRefreshToken({ user_id, verify })])
+  }
+
+  private decodeRefreshToken(refresh_token: string) {
+    return verifyToken({
+      token: refresh_token,
+      secretOrPublicKey: envConfig.jwtSecretRefreshToken
+    })
+  }
+
+  async register(payload: RegisterReqBody) {
+    const user_id = new ObjectId()
+    await databaseService.users.insertOne(
+      new User({
+        ...payload,
+        _id: user_id,
+        verify: UserVerifyStatus.Verified,
+        password: hashPassword(payload.password),
+        role: parseInt(payload.role)
+      })
+    )
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken({
+      user_id: user_id.toString(),
+      verify: UserVerifyStatus.Unverified
+    })
+    const { iat, exp } = await this.decodeRefreshToken(refresh_token)
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token, iat, exp })
+    )
+    return {
+      access_token,
+      refresh_token
+    }
+  }
+
+  async checkEmailExist(email: string) {
+    const user = await databaseService.users.findOne({ email })
+    return Boolean(user)
+  }
+
+  async getListEmail() {
+    const users = await databaseService.users.find().toArray()
+    const emails = users.map((item) => item.email)
+    return emails
+  }
+
+  async login({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken({
+      user_id,
+      verify
+    })
+    const { iat, exp } = await this.decodeRefreshToken(refresh_token)
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token, iat, exp })
+    )
+    return {
+      access_token,
+      refresh_token
+    }
+  }
+
+  async logout(refresh_token: string) {
+    await databaseService.refreshTokens.deleteOne({ token: refresh_token })
+    return {
+      message: AUTH_MESSAGES.LOGOUT_SUCCESS
+    }
+  }
+
+  async sendVerifyEmail(email: string) {
+    const subject = 'Verify email'
+    const title = 'Verify email'
+    const otp = generateRandomNumber(6)
+
+    await databaseService.verify.insertOne({
+      email,
+      otp,
+      expiresAt: new Date(Date.now() + 3 * 60 * 1000)
+    })
+
+    sendEmail({ email, subject, title, otp })
+
+    return {
+      message: AUTH_MESSAGES.SEND_VERIFY_EMAIL_SUCCESS
+    }
+  }
+
+  async verifyEmail({ email, otp }: { email: string; otp: string }) {
+    const verify = await databaseService.verify.findOneAndDelete({ email, otp })
+    return Boolean(verify)
+  }
+
+  async resendVerifyEmail(user_id: string, email: string) {
+    const email_verify_token = await this.signEmailVerifyToken({
+      user_id,
+      verify: UserVerifyStatus.Unverified
+    })
+    console.log('Resend verify email: ', email_verify_token)
+    // Gui mail o vi tri nay
+    // Cap nhat gia tri
+    await databaseService.users.updateOne(
+      {
+        _id: new ObjectId(user_id)
+      },
+      [
+        {
+          $set: {
+            email_verify_token,
+            updated_at: '$$NOW'
+          }
+        }
+      ]
+    )
+    return {
+      message: AUTH_MESSAGES.RESEND_VERIFY_EMAIL_SUCCESS
+    }
+  }
+
+  async forgotPassword({ user_id, verify, email }: { user_id: string; email: string; verify: UserVerifyStatus }) {
+    const forgot_password_token = await this.signForgotPasswordToken({ user_id, verify })
+    await databaseService.users.updateOne(
+      {
+        _id: new ObjectId(user_id)
+      },
+      [
+        {
+          $set: {
+            forgot_password_token,
+            updated_at: '$$NOW'
+          }
+        }
+      ]
+    )
+    // Gui mail tai day
+    console.log('forgot_password_token: ', forgot_password_token)
+    return {
+      message: AUTH_MESSAGES.CHECK_EMAIL_TO_RESET_PASSWORD
+    }
+  }
+
+  async resetPassword(user_id: string, password: string) {
+    databaseService.users.updateOne(
+      {
+        _id: new ObjectId(user_id)
+      },
+      {
+        $set: {
+          forgot_password_token: '',
+          password: hashPassword(password)
+        },
+        $currentDate: {
+          updated_at: true
+        }
+      }
+    )
+    return {
+      message: AUTH_MESSAGES.RESET_PASSWORD_SUCCESS
+    }
+  }
+
+  async refreshToken({
+    user_id,
+    verify,
+    refresh_token,
+    exp
+  }: {
+    user_id: string
+    verify: UserVerifyStatus
+    refresh_token: string
+    exp: number
+  }) {
+    const [new_access_token, new_refresh_token] = await Promise.all([
+      this.signAccessToken({ user_id, verify }),
+      this.signRefreshToken({ user_id, verify, exp }),
+      databaseService.refreshTokens.deleteOne({ token: refresh_token })
+    ])
+    const decoded_refresh_token = await this.decodeRefreshToken(new_refresh_token)
+    databaseService.refreshTokens.insertOne(
+      new RefreshToken({
+        user_id: new ObjectId(user_id),
+        token: new_refresh_token,
+        iat: decoded_refresh_token.iat,
+        exp: decoded_refresh_token.exp
+      })
+    )
+    return {
+      access_token: new_access_token,
+      refresh_token: new_refresh_token
+    }
+  }
+}
+
+const authService = new AuthService()
+export default authService
---
+++ backend/src/utils/handlers.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response, NextFunction, RequestHandler } from 'express'
+
+export const wrapRequestHandler = <P>(func: RequestHandler<P, any, any, any>) => {
+  return async (req: Request<P>, res: Response, next: NextFunction) => {
+    try {
+      await func(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
