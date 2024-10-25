2dff60bb0d159f75bf5b184600ad4e93b1a6dbfd
Ltta113 <letruongtuananh113@gmail.com>
2024-10-25 11:43:59 +0700

    feat: [FE][UI] change genres user page

---
+++ frontend/src/type/User/IUser.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  _id: string
+  role: number
+  name: string
+  email: string
+  gender: string
+  created_at: string
+  updated_at: string
+  verify: number
+  genres: string[]
+  avatar: string
+}
